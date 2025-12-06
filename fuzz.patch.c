cf5d5dc70dc5b2fed5d751c7f7f73bebc8d91834
Jackson Stephens <156037195+JKSNS@users.noreply.github.com>
2025-12-06 16:08:24 -0700

    Update fuzz.patch.c

--- fuzz.patch.c
+++ fuzz.patch.c
@@ -1,4 +1,4 @@
-/* AFL fuzzing helpers injected into httpd, ONE SHOT MODE */
+/* AFL fuzzing helpers injected into httpd */
 
 #include <pthread.h>
 #include <unistd.h>
@@ -10,7 +10,7 @@
 #include <stdio.h>
 
 #define FUZZING_PORT 8080
-#define MAX_INPUT_SIZE (1024 * 128)  // 128KB
+#define MAX_INPUT_SIZE (1024 * 128)  /* 128KB */
 #define CONNECT_RETRY_USEC 1000
 
 static volatile int should_exit = 0;
@@ -24,13 +24,15 @@ static void cleanup_handler(int sig) {
 }
 
 static int send_case_to_httpd(const unsigned char *buf, size_t len) {
-    int fd = socket(AF_INET, SOCK_STREAM, 0);
+    int fd;
+    struct sockaddr_in addr;
+    size_t total = 0;
+
+    fd = socket(AF_INET, SOCK_STREAM, 0);
     if (fd < 0) {
-        perror("socket");
         return -1;
     }
 
-    struct sockaddr_in addr;
     memset(&addr, 0, sizeof(addr));
     addr.sin_family = AF_INET;
     addr.sin_addr.s_addr = htonl(INADDR_LOOPBACK);
@@ -41,7 +43,6 @@ static int send_case_to_httpd(const unsigned char *buf, size_t len) {
         return -1;
     }
 
-    size_t total = 0;
     while (total < len) {
         ssize_t n = send(fd, buf + total, len - total, 0);
         if (n <= 0) {
@@ -56,30 +57,29 @@ static int send_case_to_httpd(const unsigned char *buf, size_t len) {
 }
 
 static void *fuzzer_thread(void *arg) {
+    unsigned char *buf;
+    int len;
+    int attempts = 0;
+
     (void)arg;
     signal(SIGTERM, cleanup_handler);
     signal(SIGINT, cleanup_handler);
 
     /* Get data from AFL */
-    unsigned char *buf = __AFL_FUZZ_TESTCASE_BUF;
-    int len = __AFL_FUZZ_TESTCASE_LEN;
+    buf = __AFL_FUZZ_TESTCASE_BUF;
+    len = __AFL_FUZZ_TESTCASE_LEN;
     
     if (len < 0) len = 0;
     if (len > MAX_INPUT_SIZE) len = MAX_INPUT_SIZE;
 
-    /* We try to send the data. Whether it works or fails, we exit. */
-    
-    int attempts = 0;
     while (attempts < 20) {
         if (send_case_to_httpd(buf, (size_t)len) >= 0) {
             break;
         }
-        /* Allow the listener to catch up */
         usleep(CONNECT_RETRY_USEC);
         attempts++;
     }
 
-    /* FORCE EXIT so AFL knows we are done */
     kill(getpid(), SIGTERM);
     return NULL;
 }
@@ -101,7 +101,6 @@ static void launch_fuzzy_thread(void) {
 __attribute__((constructor))
 static void start_afl_fuzzing(void) {
     if (getenv("__AFL_SHM_ID") != NULL || getenv("AFL_NO_FORKSRV") != NULL) {
-        fprintf(stderr, "[+] AFL detected, launching fuzzing thread\n");
         launch_fuzzy_thread();
     }
 }
